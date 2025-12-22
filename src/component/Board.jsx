600ea3297704dbe732add2ab7664e6e2c316c27c
amen <aamenallah593@gmail.com>
2025-12-23 00:01:46 +0100

    add rook moves

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -43,6 +43,13 @@ const Board = () => {
       }
     }
 
+    if (fromPiece.type === 'rook') {
+      if (!rookMove(dragFrom,to,turn,board)) {
+        setDragFrom(null);
+        return
+      }
+    }
+
     const newBoard = { ...board };
     newBoard[to] = fromPiece;
     newBoard[dragFrom] = null;
@@ -96,6 +103,43 @@ const Board = () => {
     }
 
     return false
+  }
+
+  const rookMove = (from,to,turn,board)=>{
+    const fromFile = from[0]
+    const fromRank = Number(from[1])
+
+    const toFile = to[0]
+    const toRank = Number(to[1])
+
+    const targetPiece = board[to];
+
+    if (fromFile !== toFile && fromRank !== toRank) {
+      return false
+    }
+
+    const fileStep =
+      fromFile === toFile ? 0 : fromFile < toFile ? 1 : -1
+    const rankStep = 
+      fromRank === toRank ? 0 : fromRank < toRank ? 1 : -1
+
+    let currentFile = fromFile.charCodeAt(0) + fileStep
+    let currentRank = fromRank + rankStep
+
+    while (currentFile !== toFile.charCodeAt(0) || currentRank !== toRank) {
+      const square = String.fromCharCode(currentFile) + currentRank
+
+      if (board[square]) return false;
+      
+      currentFile += fileStep
+      currentRank += rankStep
+    }
+
+    if (targetPiece && targetPiece.color === turn) {
+    return false;
+    }
+
+    return true
   }
 
   return (
