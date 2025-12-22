42b00d19c38fac443f611530b8c53a15ec029b6c
amen <aamenallah593@gmail.com>
2025-12-22 17:15:57 +0100

    add legal move for pawns

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -23,20 +23,80 @@ const Board = () => {
       return;
     }
 
-    const piece = board[dragFrom];
-    if (!piece || piece.color !== turn) {
+    const fromPiece = board[dragFrom];
+    if (!fromPiece || fromPiece.color !== turn) {
       setDragFrom(null);
       return;
     }
 
+    const toPiece = board[to];
+
+    if (toPiece && toPiece.color === fromPiece.color) {
+    setDragFrom(null);
+    return;
+  }
+
+    if (fromPiece.type === 'pawn') {
+      if (!pawnMove(dragFrom,to,turn,board)) {
+        setDragFrom(null);
+        return
+      }
+    }
+
     const newBoard = { ...board };
-    newBoard[to] = piece;
+    newBoard[to] = fromPiece;
     newBoard[dragFrom] = null;
 
     setboard(newBoard);
     setTurn(turn === "white" ? "black" : "white");
     setDragFrom(null);
   };
+
+  const pawnMove = (from,to,turn,board)=>{
+    const fromFile = from[0]
+    const fromRank = Number(from[1])
+
+    const toFile = to[0]
+    const toRank = Number(to[1])
+
+    const targetPiece = board[to];
+
+    const direction = turn === 'white' ? 1 : -1;
+
+    const startSquare = turn === 'white' ? 2 : 7;
+
+    const middleRank = fromRank + direction
+    const middlesquare = fromFile + middleRank
+
+    //one square
+    if (fromFile === toFile &&
+        toRank === fromRank + direction &&
+        !targetPiece
+    ) {
+      return true;
+    }
+
+    //2 square
+    if(fromRank === startSquare &&
+       fromFile === toFile &&
+       toRank === fromRank + 2 * direction &&
+       !board[middlesquare]&&
+       !targetPiece
+    ){
+      return true;
+    }
+
+    //capture
+    if (Math.abs(toFile.charCodeAt(0) - fromFile.charCodeAt(0)) === 1 &&
+        toRank === fromRank + direction &&
+        targetPiece &&
+        targetPiece.color !== turn
+    ) {
+      return true
+    }
+
+    return false
+  }
 
   return (
     <div className="grid grid-cols-8 border-2">
