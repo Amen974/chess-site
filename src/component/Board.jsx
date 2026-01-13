8d638e6c3843b53cb680c47c80a2f6ed7c919d61
amen <aamenallah593@gmail.com>
2026-01-13 14:48:11 +0100

    enhance UI responsiveness and button styling

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -26,7 +26,7 @@ const Board = () => {
 
   const [enPassantSquare, setEnPassantSquare] = useState(null);
   const [halfmoveClock, setHalfmoveClock] = useState(0);
-  const [fullmoveNumber, setFullmoveNumber] = useState(1);  
+  const [fullmoveNumber, setFullmoveNumber] = useState(1);
   const [promotion, setPromotion] = useState(null);
 
   /* ================= DRAG ================= */
@@ -67,7 +67,7 @@ const Board = () => {
     setCastlingRights(result.castlingRights);
     setEnPassantSquare(result.enPassantSquare);
     setHalfmoveClock(result.halfmoveClock);
-    setFullmoveNumber(result.fullmoveNumber)
+    setFullmoveNumber(result.fullmoveNumber);
     setPromotion(result.promotion);
 
     if (result.gameResult) {
@@ -82,20 +82,20 @@ const Board = () => {
   /* ================= Click ================= */
 
   const handleSquareClick = (square) => {
-    if(promotion) return
+    if (promotion) return;
 
-    if (!selectedSquare){
-      const piece = board[square]
-      if (!piece) return
-      if (piece.color !== turn) return
-      setSelectedSquare(square)
+    if (!selectedSquare) {
+      const piece = board[square];
+      if (!piece) return;
+      if (piece.color !== turn) return;
+      setSelectedSquare(square);
       setLegalMoves(computeLegalMoves(square));
-      return; 
+      return;
     }
-    
-    if (square === selectedSquare){
-      setSelectedSquare(null)
-      return
+
+    if (square === selectedSquare) {
+      setSelectedSquare(null);
+      return;
     }
 
     const result = applyPlayerMove({
@@ -131,23 +131,22 @@ const Board = () => {
         alert(`Draw by ${result.gameResult.reason}`);
       }
     }
-  }
+  };
 
   const computeLegalMoves = (from) => {
-  const moves = [];
+    const moves = [];
 
-  for (const r of ranks) {
-    for (const f of files) {
-      const to = f + r;
-      if (isLegalMove(from, to, board, turn, enPassantSquare)) {
-        moves.push(to);
+    for (const r of ranks) {
+      for (const f of files) {
+        const to = f + r;
+        if (isLegalMove(from, to, board, turn, enPassantSquare)) {
+          moves.push(to);
+        }
       }
     }
-  }
 
-  return moves;
-};
-
+    return moves;
+  };
 
   /* ================= PROMOTION ================= */
 
@@ -189,7 +188,7 @@ const Board = () => {
     setCastlingRights(prev.castlingRights);
     setEnPassantSquare(prev.enPassantSquare);
     setHalfmoveClock(prev.halfmoveClock);
-    setFullmoveNumber(prev.fullmoveNumber)
+    setFullmoveNumber(prev.fullmoveNumber);
 
     setHistory((h) => h.slice(0, -1));
     setRedoStack((r) => [...r, lastMove]);
@@ -219,7 +218,7 @@ const Board = () => {
     setCastlingRights(result.castlingRights);
     setEnPassantSquare(result.enPassantSquare);
     setHalfmoveClock(result.halfmoveClock);
-    setFullmoveNumber(result.fullmoveNumber)
+    setFullmoveNumber(result.fullmoveNumber);
     setPromotion(result.promotion);
 
     setHistory((h) => [...h, result.move]);
@@ -237,7 +236,7 @@ const Board = () => {
       setCastlingRights(data.castlingRights);
       setEnPassantSquare(data.enPassantSquare);
       setHalfmoveClock(data.halfmove);
-      setFullmoveNumber(data.fullmove)
+      setFullmoveNumber(data.fullmove);
 
       setHistory([]);
       setRedoStack([]);
@@ -250,60 +249,109 @@ const Board = () => {
 
   /* ================= EXPORT FEN ================= */
   const handleCopyFEN = async () => {
-  const fen = exportFEN({
-    board,
-    turn,
-    castlingRights,
-    enPassantSquare,
-    halfmoveClock,
-    fullmoveNumber,
-  });
+    const fen = exportFEN({
+      board,
+      turn,
+      castlingRights,
+      enPassantSquare,
+      halfmoveClock,
+      fullmoveNumber,
+    });
 
-  try {
-    await navigator.clipboard.writeText(fen);
-    alert("FEN copied to clipboard");
-  } catch {
-    alert("Failed to copy FEN");
-  }
-};
+    try {
+      await navigator.clipboard.writeText(fen);
+      alert("FEN copied to clipboard");
+    } catch {
+      alert("Failed to copy FEN");
+    }
+  };
 
   /* ================= RENDER ================= */
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div className="flex justify-center items-center h-screen w-screen">
+      <div className="flex flex-wrap gap-2 justify-center md:items-center">
+        <div className="grid grid-cols-8 border-4 border-grey-color rounded-2xl overflow-hidden">
+          {ranks.map((rank) =>
+            files.map((file) => {
+              const squareId = file + rank;
+              const light = isLightSquare(squareId);
 
-      <div className="grid grid-cols-8 border-4 border-grey-color rounded-2xl overflow-hidden">
-        {ranks.map((rank) =>
-          files.map((file) => {
-            const squareId = file + rank;
-            const light = isLightSquare(squareId);
+              return (
+                <Square
+                  key={squareId}
+                  id={squareId}
+                  color={light ? "blackSquare" : "whiteSquare"}
+                  piece={board[squareId]}
+                  onClick={handleSquareClick}
+                  onDragStart={handleDragStart}
+                  onDrop={handleOnDrop}
+                  isSelected={squareId === selectedSquare}
+                  isLegalMove={legalMoves.includes(squareId)}
+                />
+              );
+            })
+          )}
+        </div>
 
-            return (
-              <Square
-                key={squareId}
-                id={squareId}
-                color={light ? "blackSquare" : "whiteSquare"}
-                piece={board[squareId]}
-                onClick={handleSquareClick}
-                onDragStart={handleDragStart}
-                onDrop={handleOnDrop}
-                isSelected={squareId === selectedSquare}
-                isLegalMove={legalMoves.includes(squareId)}
-              />
-            );
-          })
+        {promotion && (
+          <PromotionModal color={promotion.color} onSelect={handlePromotion} />
         )}
+        <div>
+          <div className="w-full flex justify-center gap-1">
+            <button
+              onClick={handleUndo}
+              className="button-style transition-all active:scale-95 shadow-sm"
+            >
+              <img src="/SVG/undo.svg" alt="Undo" className="w-5 h-5" />
+              <span className="text-xs font-bold uppercase">UNDO</span>
+            </button>
+
+            <button
+              onClick={handleRedo}
+              className="button-style transition-all active:scale-95 shadow-sm"
+            >
+              <img src="/SVG/redo.svg" alt="Redo" className="w-5 h-5" />
+              <span className="text-xs font-bold uppercase">REDO</span>
+            </button>
+
+            <button className="button-style transition-all active:scale-95 shadow-sm">
+              <img src="/SVG/resign.svg" alt="Resign" className="w-5 h-5" />
+              <span className="text-xs font-bold uppercase">RESIGN</span>
+            </button>
+
+            <button className="button-style transition-all active:scale-95 shadow-sm">
+              <img src="/SVG/flip.svg" alt="Flip" className="w-5 h-5" />
+              <span className="text-xs font-bold uppercase">FLIP</span>
+            </button>
+          </div>
+
+          <div className="flex items-center gap-2 p-3 rounded-lg bg-[#1e232e] border border-slate-700 shadow-sm mt-4">
+            <div className="flex-1 relative">
+              <img
+                src="/SVG/input.svg"
+                alt="Input"
+                className="absolute left-2 top-1/2 transform -translate-y-1/2 w-4 h-4 text-slate-400"
+              />
+              <input
+                className="w-full pl-8 pr-3 py-1.5 text-xs rounded border border-slate-700 bg-[#111318] text-white placeholder-slate-400 focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary transition-shadow"
+                value={fenInput}
+                onChange={(e) => setFenInput(e.target.value)}
+                placeholder="Import FEN..."
+                onKeyDown={(e) => e.key === "Enter" && handleImportFEN()}
+              />
+            </div>
+            <button
+              className="flex items-center gap-1 px-3 py-1.5 rounded bg-[#282e39] text-xs font-semibold text-slate-300 hover:bg-[#343a46] transition-colors border border-slate-600"
+              onClick={handleCopyFEN}
+            >
+              <img src="/SVG/copy.svg" alt="Copy" className="w-4 h-4" />
+              Export FEN
+            </button>
+          </div>
+        </div>
       </div>
-
-      {promotion && (
-        <PromotionModal
-          color={promotion.color}
-          onSelect={handlePromotion}
-        />  
-      )}
-
-      
-    </div>   
+    </div>
   );
 };
 
